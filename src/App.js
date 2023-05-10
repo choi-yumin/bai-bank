75a4347367c01c9c2aaa5bc39374461379e311a9
Yumin Choi <yuminchoi@Yumins-MacBook-Pro.local>
2023-05-10 23:07:00 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import React from 'react';
+import { Box } from '@mui/material'
+import { styled } from '@mui/material'
+import './css/Title.css'
+
+
+const Rectangle1 = styled(Box)`
+  position: absolute;
+  width: 1920px;
+  height: 946px;
+  left: 0px;
+  top: 134px;
+
+  background: #F2F6FC;
+`
+const Rectangle3 = styled(Box)`
+  position: absolute;
+  width: 1463px;
+  height: 856px;
+  left: 395px;
+  top: 185px;
+
+  background: #FFFFFF;
+  border-radius: 15px;
+`
+const Rectangle6 = styled(Box)`
+  position: absolute;
+  width: 1920px;
+  height: 134px;
+  left: 0px;
+  top: 0px;
+
+  background: #FFFFFF;
+`
+
+const Title = styled(Box)`
+  position: absolute;
+  width: 738px;
+  height: 25px;
+  left: 448px;
+  top: 228px;
+
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 24px;
+  line-height: 33px;
+
+  color: #0151CA;
+`
+
+const customer = {
+  'name':'songdi',
+  'birthday':'123123'
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Rectangle6>
+      <h3>여기 로고</h3>
+    <Rectangle1>
+    <Rectangle3>
+        <Title>KAIST 학부총학생회 23년도 상반기 회계감사자료 통장거래내역</Title>
+      <Customer 
+        name={customer.name}
+        birthday={customer.birthday}/>
+    </Rectangle3>
+    </Rectangle1>
+    </Rectangle6>
   );
 }
 
